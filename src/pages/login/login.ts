a1428ab602b5a3b7636a96f165e759f18aeed8cc
unknown <hersoneder@gmail.com>
2018-04-05 06:05:18 -0500

    Login Facebook 2

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SliderPage } from '../../pages/slider/slider';
-import { Facebook } from '@ionic-native/facebook';
+import { Facebook,FacebookLoginResponse } from '@ionic-native/facebook';
 
 
 /**
@@ -24,7 +24,7 @@ export class LoginPage {
   user: any = {};
   showUser: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,private facebook: Facebook) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private facebook: Facebook) {
   }
 
 
@@ -36,29 +36,41 @@ export class LoginPage {
     console.log('ionViewDidLoad LoginPage');
   }
 
-  loginFacebook(){
-    this.facebook.login(['public_profile', 'email'])
-    .then(rta => {
-      console.log(rta.status);
-      if(rta.status == 'connected'){
-        this.getInfo();
-      };
-    })
-    .catch(error =>{
-      console.error( error );
+  loginWithFB() {
+    this.facebook.login(['email', 'public_profile']).then((response: FacebookLoginResponse) => {
+      this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
+        this.user = {email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large']['data']['url'], username: profile['name']}
+      });
     });
   }
 
-  getInfo(){
-    this.facebook.api('/me?fields=id,name,email,first_name,picture,last_name,gender',['public_profile','email'])
-    .then(data=>{
-      console.log(data);
-      this.showUser = true; 
-      this.user = data;
-    })
-    .catch(error =>{
-      console.error( error );
-    });
+  loginFacebook() {
+    try {
+      this.facebook.login(['public_profile', 'email'])
+        .then(rta => {
+          console.log(rta.status);
+          if (rta.status == 'connected') {
+            this.getInfo();
+          };
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    } catch (Error) {
+      alert(Error.message);
+    }
+  }
+
+  getInfo() {
+    this.facebook.api('/me?fields=id,name,email,first_name,picture,last_name,gender', ['public_profile', 'email'])
+      .then(data => {
+        console.log(data);
+        this.showUser = true;
+        this.user = data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
 }
