b0391d78a47dbe1543eed2e5d45d1bbe2e95e26c
unknown <hersoneder@gmail.com>
2018-05-02 04:41:03 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,7 +46,8 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.show();
-      this.handlerNotifications();
+      //Notificaciones Push
+      //this.handlerNotifications();
     });
   }
 
